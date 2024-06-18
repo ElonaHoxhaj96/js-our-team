aa6140400044e510fa1da344d66dd903ec2cc077
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-18 16:12:30 +0200

    Feat: created a cicle object with my teem information and printed in console

---
+++ main.js
@@ -0,0 +1,46 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+// creo il mio ciclo di object
+const arrayteam = [
+    {
+        "nomeECognome": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "image": "wayne-barnett-founder-ceo.jpg",
+    }, 
+    {
+        "nomeECognome": "Angela OPEN",
+        "role": "Chief Editor",
+        "image": "angela-caroll-chief-editor.jpg",
+    },
+    {
+        "nomeECognome": "Walter Gordon",
+        "role": "Office Manager",
+        "image": "walter-gordon-office-manager.jpg",
+    },
+    {
+        "nomeECognome": "Angela Lopez",
+        "role": "Social Media Manager",
+        "image": "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        "nomeECognome": "Scott Estrada",
+        "role": "Developer",
+        "image": "scott-estrada-developer.jpg",
+    },
+    {
+        "nomeECognome": "Barbara Ramos",
+        "role": "Founder & CEO",
+        "image": "barbara-ramos-graphic-designer.jpg",
+    }
+];
+console.log(arrayteam);
+  
+
